2f356c2a19d10339d062f2ad63b6dfd41e0bef49
preiser <mattpreiser@gmail.com>
2016-03-22 14:29:38 -0400

    parsed JSON object from API

--- lib/nutrition_facts/cli.rb
+++ lib/nutrition_facts/cli.rb
@@ -3,20 +3,42 @@ class NutritionFacts::CLI
     start
   end
 
+  def list
+    puts ''
+    puts 'Welcome to NutritionFacts CLI'
+    puts ''
+    @items = NutritionFacts::Item.list_items
+    @items.each.with_index(1) do |item, index|
+      puts "#{index}. - #{item.name}"
+    end
+  end
+
   def start
+    list
     input = nil
+    uri = URI.parse('https://api.nutritionix.com/v1_1/search/apple?results=0%3A5&cal_min=0&cal_max=50000&fields=item_name%2Cnf_calories%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_dietary_fiber%2Cnf_sugars%2Cnf_protein&appId=da276553&appKey=f501b79647768ba684af20428a44ef59')
+
+    response = Net::HTTP.get_response(uri)
+
+    data = JSON.parse(response.body)
+
+    data['hits'].each do |item|
+      puts item['fields']
+    end
+    
     while input != 'exit'
       puts 'Enter the name of a food to see its nutrition facts:'
       input = gets.strip.downcase
-      case input
-      when 'apple'
-        NutritionFacts::Item.item_facts
-      when 'list'
-        NutritionFacts::Item.list_items
+
+      if input.to_i > 0
+        item = @items[input.to_i - 1]
+        puts "#{item.name} - #{item.calories}"
+      elsif 'list'
+        @items
       else
         puts 'not sure what you mean, type in a food' unless input == 'exit'
-    end
+      end
 
-  end
+    end
   end
 end
--- lib/nutrition_facts/item.rb
+++ lib/nutrition_facts/item.rb
@@ -1,9 +1,26 @@
 class NutritionFacts::Item
-  attr_accessor :name, :calories, :protein, :sugars
+  attr_accessor :name, :calories, :protein, :sugar
+
+  # @@all = []
 
   def self.list_items
-    puts '1. apple raw, medium'
-    puts '2. apple cooked, large'
+    item_1 = new
+    item_1.name = 'apple raw, medium'
+    item_1.calories = '100'
+    item_1.protein = '0'
+    item_1.sugar = '10'
+    # @@all << item_1
+
+    item_2 = new
+    item_2.name = 'apple cooked, large'
+    item_2.calories = '150'
+    item_2.protein = '0'
+    item_2.sugar = '20'
+    # @@all << item_2
+
+    # @@all
+
+    [item_1, item_2]
   end
 
   def self.item_facts
@@ -14,5 +31,9 @@ class NutritionFacts::Item
   Protein:
   Sugar:
   DOC
+  end
+
+  def self.all
+    @@all
   end
 end
